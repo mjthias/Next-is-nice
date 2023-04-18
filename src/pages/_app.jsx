c64ba40da9cc1351983896ac7b572874da4504c6
Mathias Jensen <mathias.j@forteadvice.com>
2023-04-18 13:38:26 +0200

    FIC FIX

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -12,10 +12,10 @@ export default function MyApp({ Component, pageProps, navData }) {
     );
 }
 
-// MyApp.getInitialProps = async (appContext) => {
-//     // Provide the appContext, in order to do 404's
-//     const appProps = await App.getInitialProps(appContext);
-//     const res = await fetch("https://bucolic-bombolone-857476.netlify.app/api/dogs");
-//     const navData = await res.json();
-//     return { ...appProps, navData };
-// };
+MyApp.getInitialProps = async (appContext) => {
+    // Provide the appContext, in order to do 404's
+    const appProps = await App.getInitialProps(appContext);
+    const res = await fetch("https://bucolic-bombolone-857476.netlify.app/api/dogs");
+    const navData = await res.json();
+    return { ...appProps, navData };
+};
